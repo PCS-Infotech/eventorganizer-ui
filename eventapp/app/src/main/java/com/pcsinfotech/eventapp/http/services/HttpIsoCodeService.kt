aec9c979353165a503e8be20b37899ee9e352b93
Yuvaraj Mohan <ymohan@pcsinfotech.in>
2024-10-20 14:00:39 +0530

    More commits

--- eventapp/app/src/main/java/com/pcsinfotech/eventapp/http/services/HttpIsoCodeService.kt
+++ eventapp/app/src/main/java/com/pcsinfotech/eventapp/http/services/HttpIsoCodeService.kt
@@ -5,31 +5,32 @@ import com.pcsinfotech.eventapp.http.ServiceBuilder
 import com.pcsinfotech.eventapp.http.interfaces.IsocodeAPI
 import com.pcsinfotech.eventapp.http.models.HttpIsoCode
 import com.pcsinfotech.eventapp.http.models.HttpIsoCodes
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Response
 
 class HttpIsoCodeService {
-    fun getIsoCodes():ArrayList<HttpIsoCode> {
+    suspend fun getIsoCodes():ArrayList<HttpIsoCode> {
+            val api: IsocodeAPI = ServiceBuilder.buildService(IsocodeAPI::class.java)
+            var isoCodeArrayList = CoroutineScope(Dispatchers.IO).async{
+                var isoCodeArrayList: ArrayList<HttpIsoCode> = ArrayList();
+                val requestCall: Call<HttpIsoCodes> = api.getIsoCodes()
+                try {
+                    val res: Response<HttpIsoCodes> = requestCall.execute()
+                    var response: HttpIsoCodes? = res.body()
 
-        val api: IsocodeAPI = ServiceBuilder.buildService(IsocodeAPI::class.java)
-        val requestCall: Call<HttpIsoCodes> = api.getIsoCodes()
-        var isoCodeArrayList : ArrayList<HttpIsoCode> = ArrayList()
-
-        try {
-            val res: Response<HttpIsoCodes> = requestCall.execute()
-            var response: HttpIsoCodes? = res.body()
-
-            if (response != null) {
-                isoCodeArrayList = response.isoCodes
+                    if (response != null) {
+                        isoCodeArrayList = response.isoCodes
+                    }
+                } catch (ex: Exception) {
+                    Log.e("Exception", ex.toString())
+                    ex.printStackTrace();
+                }
+                return@async isoCodeArrayList;
             }
-        }
-        catch (ex: Exception)
-        {
-            Log.e("Exception", ex.toString())
-            ex.printStackTrace();
-        }
-
-        return isoCodeArrayList
+        return isoCodeArrayList.await()
     }
-
 }
--- eventapp/app/src/main/java/com/pcsinfotech/eventapp/services/IsoCodeService.kt
+++ eventapp/app/src/main/java/com/pcsinfotech/eventapp/services/IsoCodeService.kt
@@ -3,20 +3,20 @@ package com.pcsinfotech.eventapp.services
 import com.pcsinfotech.eventapp.http.models.HttpIsoCode
 import com.pcsinfotech.eventapp.http.services.HttpIsoCodeService
 import com.pcsinfotech.eventapp.models.IsoCode
+import kotlinx.coroutines.runBlocking
+import okhttp3.internal.wait
 
 
 class IsoCodeService {
     fun getIsoCodes(): ArrayList<IsoCode> {
-        var isoCodes : ArrayList<IsoCode>
-
-        //Call Http Service to get the HttpIsoCodes.
-        var httpIsoCodeService = HttpIsoCodeService()
-        var httpIsoCodes : ArrayList<HttpIsoCode> = httpIsoCodeService.getIsoCodes()
-
-        //Convert HttpIsoCodes to IsoCodes.
-        var mapper : Mapper= Mapper()
-        isoCodes = mapper.httpIsoCodesToIsoCodes(httpIsoCodes)
-
+        var isoCodes: ArrayList<IsoCode> = ArrayList()
+        runBlocking {
+            val httpIsoCodeService = HttpIsoCodeService()
+            runBlocking {
+                val httpIsoCodes = httpIsoCodeService.getIsoCodes()
+                isoCodes = Mapper().httpIsoCodesToIsoCodes(httpIsoCodes)
+            }
+        };
 
         return isoCodes
     }
